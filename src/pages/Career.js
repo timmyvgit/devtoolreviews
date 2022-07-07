e57bf51d17a678a5e576606752fd3e6911f38552
Tim Vuong <74928150+timmyvgit@users.noreply.github.com>
2022-07-07 12:46:15 -0500

    update

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,8 +1,8 @@
 export default function Career() {
   return <div>
     <h1>Career</h1>
-    <div className="row">
-      <div className="listing">
+    <div className="listing">
+
         <li><a href="https://www.linkedin.com" target="_blank">linkedin.com</a></li>
         <li><a href="https://www.indeed.com" target="_blank">indeed.com</a></li>
         <li><a href="https://www.remote.co" target="_blank">remote.co</a></li>
@@ -11,8 +11,7 @@ export default function Career() {
         <li><a href="https://www.monster.com" target="_blank">monster.com</a></li>
         <li><a href="https://www.remotehub.com" target="_blank">remotehub.com</a></li>
         <li><a href="https://www.glassdoor.com" target="_blank">glassdoor.com</a></li>
-      </div>
-      <div className="listings2">
+   
         <li><a href="https://www.hired.com" target="_blank">hired.com</a></li>
         <li><a href="https://www.salary.com" target="_blank">salary.com</a></li>
         <li><a href="https://www.flexjobs.com" target="_blank">flexjobs.com</a></li>
@@ -21,8 +20,8 @@ export default function Career() {
         <li><a href="https://www.simplyhired.com" target="_blank">simplyhired.com</a></li>
         <li><a href="https://www.reactjobs.us" target="_blank">reactjobs.us</a></li>
         <li><a href="https://www.hired.com" target="_blank">hired.com</a></li>
-      </div>
-    </div>
+
+</div>
   </div>
 
 }
